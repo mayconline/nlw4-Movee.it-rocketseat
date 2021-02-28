9b88fc24c27f09dd64da189ee4f437dd9c1ae64b
mayconline <mayconline.ti@gmail.com>
2021-02-28 12:47:19 -0300

    project created

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://i.redd.it/jkowylrw9fb21.png" alt="Developer" />
+      <div>
+        <strong>Developer</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
